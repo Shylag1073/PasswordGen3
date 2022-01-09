087e02c214d0112d522d30e94d84f921df014044
shylag1073 <shylag1073@gmail.com>
2022-01-09 11:14:18 -0600

    re-adding the functions for prompt windows

--- PasswordGen3/assets/JS/script.js
+++ PasswordGen3/assets/JS/script.js
@@ -17,19 +17,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//function generatePassword() {
+function generatePassword() {
 
-//When prompted for the length of the password
-//Then choose a length of at least 8 characters and no more than 128 characters
-// var passLength = prompt("How long would you like the password to be? Pick between 8 - 128 characters.")
-//var lowercase = confirm("Would you like to include lower case characters in the password? ")
-// var uppercase = confirm("Would you like to include upper case characters in the password")
-//var numbers = confirm("Would you like to include numbers in the password?")
-var special = confirm("Would you like special characters in your password?")
+    //When prompted for the length of the password
+    //Then choose a length of at least 8 characters and no more than 128 characters
+    var passLength = prompt("How long would you like the password to be? Pick between 8 - 128 characters.")
+    var lowercase = confirm("Would you like to include lower case characters in the password? ")
+    var uppercase = confirm("Would you like to include upper case characters in the password")
+    var numbers = confirm("Would you like to include numbers in the password?")
+    var special = confirm("Would you like special characters in your password?")
 
-    //console.log(passLength, lowercase, uppercase, numbers, special);
+    console.log(passLength, lowercase, uppercase, numbers, special);
 
-    //alert();
+    alert();
 
 
-//}
+}
